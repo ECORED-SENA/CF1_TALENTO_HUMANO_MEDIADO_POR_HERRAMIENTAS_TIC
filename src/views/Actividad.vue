851779b3c530cbb5223e2834413d11c8c40c29db
Francisco Lizcano <flizcanor@icloud.com>
2022-05-20 14:15:27 -0500

    Ajuste descripción e icono actividad

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,20 +2,20 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
+
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
-        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+        .tarjeta.h-100.d-flex.align-items-center
           figure
-            img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Actividad didáctica
+          h2 Identificar sector y actividad económica de las empresas.
         p.mb-4 Determinar el sector y la actividad económica correspondiente a las empresas seleccionadas.
         .tarjeta.tarjeta--azul.p-3
-          .row.justify-content-around.align-items-center            
+          .row.justify-content-around.align-items-center
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Identificar sector y actividad económica de las empresas.
+              p.fw-bold.mb-0 Arrastrar y soltar coincidencias.
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/index.html')" target="_blank")
                 span Realizar
